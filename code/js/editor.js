6d8c8c03cad71b501c6f685563d94aa606b8ad3b
apolol92 <apolol92@web.de>
2017-04-11 21:00:00 +0200

    Removed Buttons and Footer..

--- code/js/editor.js
+++ code/js/editor.js
@@ -1,6 +1,7 @@
 
 var markdown = require( "markdown" ).markdown;
 var styleTag = document.createElement("HTML"), flask = new CodeFlask;
+var codeBox = document.getElementById("testmich");
 
 document.querySelector('#previewField').appendChild(styleTag);
 
@@ -11,6 +12,7 @@ flask.run('#code', {
 
 flask.onUpdate(function (code) {
     styleTag.innerHTML = markdown.toHTML( code );
+    console.log(codeBox.selectionStart);
 });
 
 flask.update("### Docker\nDillinger is very easy to install and deploy in a Docker container.");
