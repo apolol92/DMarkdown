76424f1728c9531d6c9c7a6a7a2540d303c360cc
apolol92 <apolol92@web.de>
2017-04-17 21:54:54 +0200

    more comments..

--- code/js/Internationalizer.js
+++ code/js/Internationalizer.js
@@ -1,5 +1,15 @@
+/**
+ * @author Dennis Groß
+ * @date 15.04.2017
+ * This class translates the text in other languages.
+ */
 class Internationalizer {
-
+    /**
+     * 
+     * @param {which languages? (de,en,fr.. (see electron app.getLocale()))} language
+     * @param {Which languages should be used if the current language not available?} fallbackLanguage 
+     * @param {Dictionary in JSON-Format} jsonDictionary 
+     */
     constructor(language, fallbackLanguage, jsonDictionary) {
         this.LANGUAGE_NOT_SUPPORTED = "LANGUAGE_NOT_SUPPORTED";
         this.WRONG_LABEL = "WRONG_LABEL";
@@ -7,13 +17,17 @@ class Internationalizer {
         this._fallbackLanguage = fallbackLanguage;
         this.jsonDictionary = JSON.parse(jsonDictionary);
     }
-
+    /**
+     * Prints the label in the current language
+     * @param {which label should be printed..=} label_name 
+     * @return translated label
+     */
     print(label_name) {
         if (this.jsonDictionary[label_name] === undefined) {
             return this.WRONG_LABEL;
         }
         else if (this.jsonDictionary[label_name][this._language] === undefined) {
-            if(this.jsonDictionary[label_name][this._fallbackLanguage]===undefined) {
+            if (this.jsonDictionary[label_name][this._fallbackLanguage] === undefined) {
                 return this.LANGUAGE_NOT_SUPPORTED;
             }
             return this.jsonDictionary[label_name][this._fallbackLanguage];
@@ -23,9 +37,13 @@ class Internationalizer {
         }
     }
 
+    /**
+     * Get current language..
+     * @return the current language
+     */
     getLanguage() {
         return this._language;
     }
 }
-
+//Export Intenationlizer..
 module.exports.Internationalizer = Internationalizer;
